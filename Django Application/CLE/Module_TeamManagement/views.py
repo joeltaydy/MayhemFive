92f7540e709ab0df29a2150224349b4468435963
Al <faried_94@hotmail.com>
2018-06-15 01:26:36 +0800

    Update Module_TeamManagement: Set up login.html and home.html pages

--- Django Application/CLE/Module_TeamManagement/views.py
+++ Django Application/CLE/Module_TeamManagement/views.py
@@ -3,4 +3,8 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
-# Create your views here.
+def login(requests):
+    return render(requests,"login.html",{})
+
+def home(requests):
+    return render(requests,"home.html",{})
