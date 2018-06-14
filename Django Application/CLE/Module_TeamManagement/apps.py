299ac3ab5c903e2040bb868e62e913014b919427
Al <faried_94@hotmail.com>
2018-06-15 01:10:00 +0800

    Add Team Management Module app to the project

---
+++ Django Application/CLE/Module_TeamManagement/apps.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class ModuleTeammanagementConfig(AppConfig):
+    name = 'Module_TeamManagement'
