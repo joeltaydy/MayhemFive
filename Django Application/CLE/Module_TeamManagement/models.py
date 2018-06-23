ad03f644c4c579bd5a8433f3f8c526e89fadb5a6
Al <faried_94@hotmail.com>
2018-06-23 20:47:49 +0800

    Edit models.py

--- Django Application/CLE/Module_TeamManagement/models.py
+++ Django Application/CLE/Module_TeamManagement/models.py
@@ -5,24 +5,40 @@ from django.db import models
 
 # Create your models here.
 class Instructor(models.Model):
-    email = models.CharField(max_length=255, primary_key=True)
-    username = models.CharField(max_length=255)
-    firstname = models.CharField(max_length=255)
-    lastname = models.CharField(max_length=255)
-    password = models.CharField(max_length=255)
+    email = models.CharField(db_column='Email', max_length=255, primary_key=True)
+    username = models.CharField(db_column='Username', max_length=255)
+    firstname = models.CharField(db_column='Firstname', max_length=255)
+    lastname = models.CharField(db_column='Lastname', max_length=255)
+    password = models.CharField(db_column='Password', max_length=255)
+
+    class Meta:
+        managed = True
+        db_table = 'Instructor'
 
 class Section(models.Model):
-    section_number = models.CharField(max_length=255, primary_key=True)
+    section_number = models.CharField(db_column='Section_Number', max_length=255, primary_key=True)
+
+    class Meta:
+        managed = True
+        db_table = 'section'
 
 class Team(models.Model):
-    team_number = models.CharField(max_length=255)
-    section_number = models.ForeignKey(Section, on_delete=models.CASCADE)
-    # How to do composite primary key (section_number, team_number)?
+    team_number = models.CharField(db_column='Team_Number', max_length=255)
+    section_number = models.ForeignKey(Section, on_delete=models.CASCADE, db_column='Section_Number')
+
+    class Meta:
+        managed = True
+        db_table = 'Team'
+        unique_together = (('section_number','team_number'),)
 
 class Section_Instructor(models.Model):
-    section_number = models.ForeignKey(Section, on_delete=models.CASCADE)
-    email = models.ForeignKey(Instructor, on_delete=models.CASCADE)
-    # How to do composite primary key (section_number, email)?
+    section_number = models.ForeignKey(Section, on_delete=models.CASCADE, db_column='Section_Number')
+    instructor_email = models.ForeignKey(Instructor, on_delete=models.CASCADE, db_column='Instructor_Email')
+
+    class Meta:
+        managed = True
+        db_table = 'Section_Instructor'
+        unique_together = (('section_number', 'instructor_email'),)
 
 class Student(models.Model):
     email = models.CharField(max_length=255, primary_key=True)
@@ -30,4 +46,9 @@ class Student(models.Model):
     firstname = models.CharField(max_length=255)
     lastname = models.CharField(max_length=255)
     password = models.CharField(max_length=255)
-    # How to do reference composite primary key from table Team(section_number, team_number)?
+#    section_number = models.ForeignKey(Team, on_delete=models.CASCADE, db_column='Section_Number')
+#    team_number = models.ForeignKey(Team, on_delete=models.CASCADE, db_column='Team_number')
+
+    class Meta:
+        managed = True
+        db_table = 'Student'
