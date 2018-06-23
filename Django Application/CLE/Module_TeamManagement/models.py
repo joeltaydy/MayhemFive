b12fc580a717067f427c6f4acc302cd487034868
Al <faried_94@hotmail.com>
2018-06-23 14:06:08 +0800

    Set up models.py (Note: run 'python manage.py migrate')

--- Django Application/CLE/Module_TeamManagement/models.py
+++ Django Application/CLE/Module_TeamManagement/models.py
@@ -4,3 +4,30 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Instructor(models.Model):
+    email = models.CharField(max_length=255, primary_key=True)
+    username = models.CharField(max_length=255)
+    firstname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+
+class Section(models.Model):
+    section_number = models.CharField(max_length=255, primary_key=True)
+
+class Team(models.Model):
+    team_number = models.CharField(max_length=255)
+    section_number = models.ForeignKey(Section, on_delete=models.CASCADE)
+    # How to do composite primary key (section_number, team_number)?
+
+class Section_Instructor(models.Model):
+    section_number = models.ForeignKey(Section, on_delete=models.CASCADE)
+    email = models.ForeignKey(Instructor, on_delete=models.CASCADE)
+    # How to do composite primary key (section_number, email)?
+
+class Student(models.Model):
+    email = models.CharField(max_length=255, primary_key=True)
+    username = models.CharField(max_length=255)
+    firstname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    # How to do reference composite primary key from table Team(section_number, team_number)?
