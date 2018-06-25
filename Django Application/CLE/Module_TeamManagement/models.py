57b60fd5cfce3f9a8f0a90129c8963df43ea8de5
Al <faried_94@hotmail.com>
2018-06-25 14:46:54 +0800

    Update ER Diagram

--- Django Application/CLE/Module_TeamManagement/models.py
+++ Django Application/CLE/Module_TeamManagement/models.py
@@ -5,49 +5,39 @@ from django.db import models
 
 # Create your models here.
 class Instructor(models.Model):
-    email = models.CharField(db_column='Email', max_length=255, primary_key=True)
+    email = models.EmailField(db_column='Instructor_Email', primary_key=True)
     username = models.CharField(db_column='Username', max_length=255)
     firstname = models.CharField(db_column='Firstname', max_length=255)
     lastname = models.CharField(db_column='Lastname', max_length=255)
     password = models.CharField(db_column='Password', max_length=255)
+    section_number = models.ManyToManyField(Section, db_column='Section_Number', null=True)
 
     class Meta:
         managed = True
         db_table = 'Instructor'
 
 class Section(models.Model):
-    section_number = models.CharField(db_column='Section_Number', max_length=255, primary_key=True)
+    section_number = models.CharField(db_column='Section_Number', max_length=2, primary_key=True)
 
     class Meta:
         managed = True
-        db_table = 'section'
+        db_table = 'Section'
 
-class Team(models.Model):
-    team_number = models.CharField(db_column='Team_Number', max_length=255)
+class Assigned_Team(models.Model):
+    student_email = models.ForeignKey(Student, on_delete=models.CASCADE, db_column='Student_Email', primary_key=True)
+    team_number = models.CharField(db_column='Team_Number', max_length=2)
     section_number = models.ForeignKey(Section, on_delete=models.CASCADE, db_column='Section_Number')
 
     class Meta:
         managed = True
-        db_table = 'Team'
-        unique_together = (('section_number','team_number'),)
-
-class Section_Instructor(models.Model):
-    section_number = models.ForeignKey(Section, on_delete=models.CASCADE, db_column='Section_Number')
-    instructor_email = models.ForeignKey(Instructor, on_delete=models.CASCADE, db_column='Instructor_Email')
-
-    class Meta:
-        managed = True
-        db_table = 'Section_Instructor'
-        unique_together = (('section_number', 'instructor_email'),)
+        db_table = 'Assigned_Team'
 
 class Student(models.Model):
-    email = models.CharField(max_length=255, primary_key=True)
-    username = models.CharField(max_length=255)
-    firstname = models.CharField(max_length=255)
-    lastname = models.CharField(max_length=255)
-    password = models.CharField(max_length=255)
-#    section_number = models.ForeignKey(Team, on_delete=models.CASCADE, db_column='Section_Number')
-#    team_number = models.ForeignKey(Team, on_delete=models.CASCADE, db_column='Team_number')
+    email = models.EmailField(db_column='Student_Email', primary_key=True)
+    username = models.CharField(db_column='Username', max_length=255)
+    firstname = models.CharField(db_column='Firstname', max_length=255)
+    lastname = models.CharField(db_column='Lastname', max_length=255)
+    password = models.CharField(db_column='Password', max_length=255)
 
     class Meta:
         managed = True
