06d3cac2f22bdc95415930b22f7269fdfc16cfc2
Al <faried_94@hotmail.com>
2018-06-27 00:30:03 +0800

    Updated Module_TeamManagement.urls.py file to only include files within app

--- Django Application/CLE/Module_TeamManagement/urls.py
+++ Django Application/CLE/Module_TeamManagement/urls.py
@@ -3,9 +3,5 @@ import views
 
 urlpatterns = [
     url(r'^home/', views.home, name='home'),
-    url(r'^$', views.home, name ='index' ),
-    url(r'^login/', views.login, name='login'),
-    url(r'^login/loading', views.login_validation, name='login_validation'),
     url(r'^upload/csv/', views.uploadcsv, name='uploadcsv'),
-    url(r'^upload/csv/loading',views.upload_csv,name='upload_csv'),
 ]
