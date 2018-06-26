0e5e6f573d8d68ff2796205e15cece48a54ba437
Al <faried_94@hotmail.com>
2018-06-27 00:33:29 +0800

    Edit bootstrap.py to do a full bootstrap into student, section and Assigned_Team table

--- Django Application/CLE/Module_TeamManagement/src/bootstrap.py
+++ Django Application/CLE/Module_TeamManagement/src/bootstrap.py
@@ -1,62 +1,78 @@
-# coding: utf-8
 import xlrd
-import time
-import csv
-import os,sys
-from openpyxl import load_workbook
-from io import BytesIO
-from django.db import connection
 from django.core.files import File
-
-#import ValidateDAL
+from Module_TeamManagement.models import Section, Student, Instructor, Assigned_Team
 
 #-----------------------------------------------------------------------------#
 #-------------------------- Bootstrap Function -------------------------------#
 #-----------------------------------------------------------------------------#
 
-def bootstrapData(classFile):
+def parse_Excel(classFile):
+    teamManagementDict = {}
 
+    # Create a workbook object from the classFile
     workbook = xlrd.open_workbook(classFile.temporary_file_path())
-    sheet = workbook.sheet_by_index(0) #first worksheet
 
-    #header = sheet.row_values(0) # header 
-    #print header
+    # Get first worksheet
+    sheet = workbook.sheet_by_index(0)
 
-    studentData = []
+    # Start with '1' instead of '0' to clear header buffer
+    for rowx in xrange(1,sheet.nrows):
 
-    for rowx in xrange(1,sheet.nrows): # clear header buffer
+        # Row data is in this format:
+        # ['Email' , u'Username', u'Last Name', u'First Name', u'Email', u'Section', u'Project G1', u'Project G2', u'Project G3', u'Project G4', u'Project G5', u'Project G6', u'Project G7']
         rowData = sheet.row_values(rowx)
-        studentParticulars = [] # each student information in this format [EMAIL, USERNAME, FIRST NAME, LAST NAME, PASSWORD, SECT NO, TEAM NO]
-        # Row data is in this format ['Email' , u'Username', u'Last Name', u'First Name', u'Email', u'Section', u'Project G1', u'Project G2', u'Project G3', u'Project G4', u'Project G5', u'Project G6', u'Project G7']
-        userName = rowData[0]
-        if "smustu\\" in userName:
-            userName = userName[6:]
-        
-        studentParticulars.extend([rowData[3],userName,rowData[2],rowData[1]])
-        
-        # default password can be changed here 
-        password = userName.upper()+rowData[2].lower()+"_ESM1001"
-        #print password
-        studentParticulars.append(password)
-        
-        #Section and team information
         newList = list(filter(None,rowData[5:]))
-        sectionTeam = [newList[0][:3],newList[0][3:]]
-        studentParticulars.extend(sectionTeam)
-        
-        studentData.append(studentParticulars)
 
-    with open('studentProcessed.csv','wb') as csvFile:
-        writer = csv.writer(csvFile)
-        writer.writerow(["EMAIL", "USERNAME", "FIRST NAME", "LAST NAME", "PASSWORD", "SECT NO"," TEAM NO"])
-        for row in studentData:
-            writer.writerow(row)
-            
+        # Declare variables
+        username = rowData[0]
+        if "smustu\\" in username:
+            username = username.split("\\")[1]
 
+        email = rowData[3]
+        firstname = rowData[2]
+        lastname = rowData[1]
+        password = "temp12345"
+        section_number = newList[0][:3]
+        team_number = newList[0][3:]
 
-    cursor = connection.cursor()
-    cursor.execute("LOAD DATA LOCAL INFILE 'studentProcessed.csv' INTO TABLE Student FIELDS TERMINATED BY ',' IGNORE 1 LINES")
-    os.remove('studentProcessed.csv') # delete the file afterwards
+        # Create student : list
+        student = [email,username,firstname,lastname,password,team_number]
 
+        # Store in dict with section_number as key and student : list as value
+        if section_number not in teamManagementDict.keys():
+            teamManagementDict[section_number] = [student]
+        else:
+            teamManagementDict[section_number].append(student)
 
-    return 
+    return teamManagementDict
+
+def clearDB():
+    Section.objects.all().delete()
+    Student.objects.all().delete()
+    Assigned_Team.objects.all().delete()
+
+def bootstrap(classFile):
+    clearDB()
+    teamManagementDict = parse_Excel(classFile)
+
+    # Bootstrap data into database
+    for section in teamManagementDict.keys():
+        sectionObj = Section.objects.create(section_number=section)
+        studentList = teamManagementDict[section]
+
+        for student in studentList:
+            studentObj = Student.objects.create(
+                email=student[0],
+                username=student[1],
+                firstname=student[2],
+                lastname=student[3],
+                password=student[4],
+            )
+            print(student[5])
+            team = Assigned_Team.objects.create(
+                student=studentObj,
+                team_number=student[5],
+                section=sectionObj,
+            )
+
+            team.save()
