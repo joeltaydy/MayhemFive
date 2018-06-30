66f19159db86cb0e6712e06c43ea8ec898338200
joel <joel.tay.2016@sis.smu.edu.sg>
2018-06-30 16:21:44 +0800

    Finish Simple student management module , instructor module. Basic login and logout without session object

--- Django Application/CLE/Module_Account/urls.py
+++ Django Application/CLE/Module_Account/urls.py
@@ -4,4 +4,5 @@ import views
 urlpatterns = [
     url(r'^$', views.login, name='login'),
     url(r'^login/', views.login, name='login'),
+    url(r'^logout/', views.logout, name='logout')
 ]
--- Django Application/CLE/Module_Account/views.py
+++ Django Application/CLE/Module_Account/views.py
@@ -8,18 +8,33 @@ from Module_Account.src import validate
 import traceback
 
 def login(requests):
+    from django.shortcuts import redirect
     if requests.method == "GET":
         return render(requests, "login.html", {})
-
+    result = {}
     # If not GET, then proceed
     try:
         username = requests.POST.get("username")
         password = requests.POST.get("password")
+
+
+        '''
+        Default login for testing purpose
+        '''
+        if(username == "admin" and password == "admin123"):
+            return redirect('/home/')
 
         # Proceed to validating of username and password
         result = validate.validate(username,password)
 
     except Exception as e:
         return render(requests, "login.html", {"error" : str(e)})
+    if result["status"] == "admin":
+        return render(requests, "Instructor/instructorOverview.html", result)
+    else: 
+        #HttpResponseRedirect(('TMmod:home'))
+        return render(requests, "Student/studentHome.html", result)
 
-    return render(requests, "login.html", result)
+def logout(requests):
+    from django.shortcuts import redirect
+    return redirect('/login/')
