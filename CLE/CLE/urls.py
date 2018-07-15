a4f8422d39eaea1157f4657f490e11ef59120d61
Al <faried_94@hotmail.com>
2018-07-15 19:30:12 +0800

    Fix bug with include method for urls.py

--- CLE/CLE/urls.py
+++ CLE/CLE/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('Module_TeamManagement.urls', namespace="TMmod")),
-    path('', include('Module_Account.urls', namespace="Amod")),
+    path('', include(('Module_TeamManagement.urls','Module_TeamManagement'), namespace="TMmod")),
+    path('', include(('Module_Account.urls','Module_Account'), namespace="Amod")),
 ]
