9432fb403eafd8d97b2fa8b128e4350b8dd51671
Al <faried_94@hotmail.com>
2018-07-28 14:41:00 +0800

    Alter bootstrap.py to parse excel specifically for student, instrustor and teaching assistant

--- CLE/Module_TeamManagement/src/bootstrap.py
+++ CLE/Module_TeamManagement/src/bootstrap.py
@@ -1,96 +1,242 @@
 import xlrd
 from django.core.files import File
-from Module_TeamManagement.models import Section, Student, Instructor, Assigned_Team
+from Module_TeamManagement.models import Section, Student, Instructor, Assigned_Team#, Teaching_Assistant
 
 #-----------------------------------------------------------------------------#
 #-------------------------- Bootstrap Function -------------------------------#
 #-----------------------------------------------------------------------------#
 
-def parse_Excel(classFile):
-    teamManagementDict = {}
+def parse_Excel_Student(file,courseInfo={}):
 
-    # Create a workbook object from the classFile
-    workbook = xlrd.open_workbook(classFile.temporary_file_path())
+    # Create a workbook object from the file
+    workbook = xlrd.open_workbook(file.temporary_file_path())
 
     # Get first worksheet
     sheet = workbook.sheet_by_index(0)
+
+    # Get headers
+    headers = sheet.row_values(0)
+
+    # Get header indexes of each column
+    index_username = headers.index('Username')
+    index_lastname = headers.index('Last Name')
+    index_firstname = headers.index('First Name')
+    index_email = headers.index('Email')
+    index_section = headers.index('Section')
 
     # Start with '1' instead of '0' to clear header buffer
     for row in range(1,sheet.nrows):
 
         # Row data is in this format:
-        # ['Email' , u'Username', u'Last Name', u'First Name', u'Email', u'Section', u'Project G1', u'Project G2', u'Project G3', u'Project G4', u'Project G5', u'Project G6', u'Project G7']
+        # [u'Username', u'Last Name', u'First Name', u'Email', u'Section', u'Project G1', u'Project G2', u'Project G3', u'Project G4', u'Project G5', u'Project G6', u'Project G7']
         rowData = sheet.row_values(row)
-        newList = list(filter(None,rowData[5:]))
+        teamList = rowData[index_section+1:]
 
         # Declare variables
-        username = rowData[0]
-        if "smustu\\" in username:
+        username = rowData[index_username].strip()
+        if '\\' in username:
             username = username.split("\\")[1]
 
-        email = rowData[3]
-        firstname = rowData[2]
-        lastname = rowData[1]
-        password = "temp12345"
-        section_number = newList[0][:3]
-        team_number = newList[0][3:]
+        section_number = rowData[index_section].strip()
+        if ',' in section_number:
+            section_number = section_number.split(",")[1]
+
+        email = rowData[index_email].strip()
+        firstname = rowData[index_firstname].strip()
+        lastname = rowData[index_lastname].strip()
+        team_number = 'T' + list(filter(None,teamList))[0].split()[-1]
 
         # Create student : list
-        student = [email,username,firstname,lastname,password,team_number]
+        student = [email,username,firstname,lastname,team_number]
 
         # Store in dict with section_number as key and student : list as value
-        if section_number not in teamManagementDict.keys():
-            teamManagementDict[section_number] = [student]
-        else:
-            teamManagementDict[section_number].append(student)
+        try:
+            courseInfo[section_number]['student'].append(student)
+        except Exception as ex:
+            if ex.args[0] == section_number:
+                courseInfo[section_number] = {'student':[student]}
+            elif ex.args[0] == 'student':
+                courseInfo[section_number].update({'student':[student]})
 
-    return teamManagementDict
+    return courseInfo
 
-def create_Group(courseInfo):
-    return
 
-def clearDB():
+def parse_Excel_Instructor(file,courseInfo={}}):
+
+    # Create a workbook object from the file
+    workbook = xlrd.open_workbook(file.temporary_file_path())
+
+    # Get first worksheet
+    sheet = workbook.sheet_by_index(0)
+
+    # Get headers
+    headers = sheet.row_values(0)
+
+    # Get header indexes of each column
+    index_username = headers.index('Username')
+    index_lastname = headers.index('Last Name')
+    index_firstname = headers.index('First Name')
+    index_email = headers.index('Email')
+    index_section = headers.index('Section') # Assume that there are more than one section and they are seperated by commas
+
+    # Start with '1' instead of '0' to clear header buffer
+    for row in range(1,sheet.nrows):
+
+        # Row data is in this format:
+        # [u'Username', u'Last Name', u'First Name', u'Email', u'Section']
+        rowData = sheet.row_values(row)
+
+        # Declare variables
+        username = rowData[index_username].strip()
+        if '\\' in username:
+            username = username.split("\\")[1]
+
+        section_number_list = rowData[index_section].strip().split(",")
+        email = rowData[index_email].strip()
+        firstname = rowData[index_firstname].strip()
+        lastname = rowData[index_lastname].strip()
+
+        # Create instructor : list
+        instructor = [email,username,firstname,lastname]
+
+        # Store in dict with section_number as key and instructor : list as value
+        for section_number in section_number_list:
+            try:
+                courseInfo[section_number]['instructor'].append(instructor)
+            except Exception as ex:
+                if ex.args[0] == section_number:
+                    courseInfo[section_number] = {'instructor':[instructor]}
+                elif ex.args[0] == 'instructor':
+                    courseInfo[section_number].update({'instructor':[instructor]})
+
+    return courseInfo
+
+
+def parse_Excel_Assistant(file,courseInfo={}):
+
+    # Create a workbook object from the classFile
+    workbook = xlrd.open_workbook(file.temporary_file_path())
+
+    # Get first worksheet
+    sheet = workbook.sheet_by_index(0)
+
+    # Get headers
+    headers = sheet.row_values(0)
+
+    # Get header indexes of each column
+    index_username = headers.index('Username')
+    index_lastname = headers.index('Last Name')
+    index_firstname = headers.index('First Name')
+    index_email = headers.index('Email')
+    index_section = headers.index('Section')
+
+    # Start with '1' instead of '0' to clear header buffer
+    for row in range(1,sheet.nrows):
+
+        # Row data is in this format:
+        # [u'Username', u'Last Name', u'First Name', u'Email', u'Section']
+        rowData = sheet.row_values(row)
+
+        # Declare variables
+        username = rowData[index_username].strip()
+        if '\\' in username:
+            username = username.split("\\")[1]
+
+        section_number = rowData[index_section].strip()
+        if ',' in section_number:
+            section_number = section_number.split(',')[-1]
+
+        email = rowData[index_email].strip()
+        firstname = rowData[index_firstname].strip()
+        lastname = rowData[index_lastname].strip()
+
+        # Create teaching_assistant : list
+        teaching_assistant = [email,username,firstname,lastname]
+
+        # Store in dict with section_number as key and teaching_assistant : list as value
+        try:
+            courseInfo[section_number]['teaching_assistant'].append(teaching_assistant)
+        except Exception as ex:
+            if ex.args[0] == section_number:
+                courseInfo[section_number] = {'teaching_assistant':[teaching_assistant]}
+            elif ex.args[0] == 'teaching_assistant':
+                courseInfo[section_number].update({'teaching_assistant':[teaching_assistant]})
+
+    return courseInfo
+
+
+def clear_Database():
     Section.objects.all().delete()
     Student.objects.all().delete()
     Assigned_Team.objects.all().delete()
+    Instructor.objects.all().delete()
+    Teaching_Assistant.objects.all().delete()
 
+
+# TO-DO: Reconfigure model to accept this format; Currently not configured to accept Teaching_Assistant Model
 # Format of dictionary:
 # Section
 #   |- Student
-#       |- [email,username,firstname,lastname,password]
-#       |- [email,username,firstname,lastname,password]
-#       |- [email,username,firstname,lastname,password]
+#       |- [email,username,firstname,lastname]
+#       |- [email,username,firstname,lastname]
+#       |- [email,username,firstname,lastname]
 #       |- ...
 #   |- Instructor
-#       |- [email,username,firstname,lastname,password,section]
-#       |- [email,username,firstname,lastname,password,section]
-#       |- [email,username,firstname,lastname,password,section]
+#       |- [email,username,firstname,lastname,section]
+#       |- [email,username,firstname,lastname,section]
+#       |- [email,username,firstname,lastname,section]
+#       |- ...
+#   |- Teaching_Assistant
+#       |- [email,username,firstname,lastname,section]
+#       |- [email,username,firstname,lastname,section]
+#       |- [email,username,firstname,lastname,section]
 #       |- ...
 #
-def bootstrap(classFile):
-    clearDB()
-    teamManagementDict = parse_Excel(classFile)
+def bootstrap(fileDict):
+    if fileDict['type'] == 'excel':
+        courseInfo = parse_Excel_Student(fileDict['file']) if fileDict['user'] == 'student' else parse_Excel_Instructor(fileDict['file'])
+    else:
+        try:
+            courseInfo = parse_Excel_Student(fileDict['file_student'])
+            courseInfo = parse_Excel_Instructor(fileDict['file_instructor'],courseInfo)
+        except:
+            raise Exception(".zip file does not contain the required excel files.")
+        clear_Database()
 
     # Bootstrap data into database
-    for section in teamManagementDict.keys():
+    for section,sectionData in courseInfo.iteritems():
         sectionObj = Section.objects.create(section_number=section)
-        studentList = teamManagementDict[section]
 
-        for student in studentList:
-            studentObj = Student.objects.create(
-                email=student[0],
-                username=student[1],
-                firstname=student[2],
-                lastname=student[3],
-                password=student[4],
-            )
+        for userType,data in sectionData.iteritems():
+            if userType == 'student':
+                studentList = courseInfo[section]['student']
 
-            team = Assigned_Team.objects.create(
-                student=studentObj,
-                team_number=student[5],
-                section=sectionObj,
-            )
+                for student in studentList:
+                    studentObj = Student.objects.create(
+                        email=student[0],
+                        username=student[1],
+                        firstname=student[2],
+                        lastname=student[3],
+                    )
 
-            team.save()
+                    team = Assigned_Team.objects.create(
+                        student=studentObj,
+                        team_number=student[5],
+                        section=sectionObj,
+                    )
 
-    create_Group(teamManagementDict)
+                    team.save()
+
+            else:
+                instructorList = courseInfo[section]['instructor']
+
+                for instructor in instructorList:
+                    instructorObj = Instructor.objects.create(
+                        email=student[0],
+                        username=student[1],
+                        firstname=student[2],
+                        lastname=student[3],
+                        section=sectionObj,
+                    )
+
+                    instructorObj.save()
