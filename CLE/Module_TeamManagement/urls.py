09cbff76ed39ee920c9ae1eba276ce17a66b52c2
Al <faried_94@hotmail.com>
2018-08-13 20:28:06 +0800

    Revert changes to master repo version

--- CLE/Module_TeamManagement/urls.py
+++ CLE/Module_TeamManagement/urls.py
@@ -5,11 +5,13 @@ from django.urls import path
 urlpatterns = [
     path('home/', views.home, name='home'),
     # path('instructor/uploadcsv/', views.uploadcsv, name='uploadcsv'),
-    path('instructor/uploadcsv/', views.configureDB_students, name='uploadcsv'),
+    path('instructor/configStudents/', views.configureDB_students, name='uploadcsv'),
     path('student/team/', views.student_Team, name='sTeam'),
     path('student/stats/', views.studStats, name='sStats'),
     path('student/profile/', views.student_Profile, name='sProfile'),
     path('instructor/team/', views.faculty_Overview, name='instOverview'),
     path('instructor/profile/', views.faculty_Profile, name='instProfile'),
     path('instructor/home/', views.faculty_Home, name='instHome'),
+    path('instructor/notiMgmt/', views.ntmgmt, name = 'noti_mgmt' ),
+    path('CLEAdmin/',views.CLEAdmin, name = 'cleAdmin'),
 ]
