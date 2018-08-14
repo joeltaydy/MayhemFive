ebe9be6f45087f4340ea75f474ff8c32ec2fb43c
Al <faried_94@hotmail.com>
2018-08-14 15:59:47 +0800

    FIX BUG

--- CLE/Module_TeamManagement/src/utilities.py
+++ CLE/Module_TeamManagement/src/utilities.py
@@ -9,19 +9,19 @@ from Module_TeamManagement.models import *
 Populate relevant courses related to instructors/students from database
 '''
 def populateRelevantCourses(requests,instructorEmail=None,studentEmail=None):
+    courseList = {}
+
     if instructorEmail != None:
         courseObject = Faculty.objects.get(email=instructorEmail).course_section.all()
+        for course in courseObject:
+            courseList[course.course_section_id] = course.course.course_title + " " + course.section_number
+
     elif studentEmail != None:
         classObject = Class.objects.all().filter(student=studentEmail).distinct()
-        for individualClass in classObject:
-            try:
-                courseObject.append(individualClass)
-            except:
-                courseObject = [individualClass]
+        for individuaClass in classObject:
+            course_section = individuaClass.course_section
+            courseList[course.course_section_id] = course.course.course_title + " " + course.section_number
 
-    courseList = {}
-    for course in courseObject:
-        courseList[course.course_section_id] = course.course.course_title + " " + course.section_number
 
     requests.session['courseList'] = courseList
     return
