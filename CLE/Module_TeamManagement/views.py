8f6a57670311493d9de17a8a6ce167fd7b23c6ea
joel <joel.tay.2016@sis.smu.edu.sg>
2018-08-25 14:40:49 +0800

    Commit last night changes for bug fixes

--- CLE/Module_TeamManagement/views.py
+++ CLE/Module_TeamManagement/views.py
@@ -11,7 +11,7 @@ from allauth.socialaccount.models import SocialAccount
 
 from random import randint
 from django.views.generic import TemplateView
-from chartjs.views.lines import BaseLineChartView
+
 
 # Student Home Page
 #@login_required(login_url='/')
@@ -679,5 +679,7 @@ def configure_telegram(requests):
 
     response['message'] = 'Telegram Group Configured'
     return render(requests, "Module_TeamManagement/Instructor/instructorTools.html", response)
+
+
 
 line_chart = TemplateView.as_view(template_name='Module_TeamManagement\line_chart.html')
