dea83ab6e31386d1f4c01120641df2d194d3af6b
Al <faried_94@hotmail.com>
2018-08-26 18:10:10 +0800

    Update config file for telegram utilities functions

--- CLE/Module_TeamManagement/src/tele_config.py
+++ CLE/Module_TeamManagement/src/tele_config.py
@@ -3,7 +3,7 @@ import os
 # telethon configuration values
 API_ID = '367454'
 API_HASH = '1bf84fb9cec9b739bc9dc2a5fe97ee10'
-SESSION_FOLDER = os.path.abspath('telegram_sessions')
+SESSION_FOLDER = os.path.join(os.getcwd(),'Module_TeamManagement/src/telegram_sessions')
 ADMIN_SESSION_FILE = 'admin_login.session'
 ADMIN_USERNAME = 'admin_login'
 
