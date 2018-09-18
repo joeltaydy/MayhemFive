d13d5d9c5e9ad2c8e0b752bd8b50b9ed657ac945
Al <faried_94@hotmail.com>
2018-09-18 23:06:52 +0800

    Trying different encodings

--- CLE/Module_TeamManagement/src/utilities.py
+++ CLE/Module_TeamManagement/src/utilities.py
@@ -388,7 +388,7 @@ def webScrapper_SingleLink(student_email,link):
     stats = soup.find_all('div', attrs={'class', 'user-information__achievements-data'})
 
     if os.path.isfile(output_file):
-        with open(output_file, mode='r', encoding='utf-8') as inputFile:
+        with open(output_file, mode='r', encoding='utf16') as inputFile:
             reader = csv.reader(inputFile, delimiter=',')
             for row in reader:
                 if row[1] != student_email:
@@ -408,11 +408,9 @@ def webScrapper_SingleLink(student_email,link):
         ]
     )
 
-    with (open(output_file, mode='w', newline='')) as outputFile:
-        writer = csv.writer(outputFile)
-        print(type(content))
+    with open(output_file, mode='w', encoding='utf8') as outputFile:
+        writer = csv.writer(outputFile, delimiter=',')
         for row in content:
-            print(type(row))
             writer.writerow(row)
 
 
