7bdf1bf62db010d7347c9242c4b34921c998dfb9
Martin Teo <martin.teo.2016@smu.edu.sg>
2018-09-21 13:32:16 +0800

    Revert "Merge branch 'master' of https://github.com/joeltaydy/MayhemFive"
    This reverts commit fc3499aa4c4badf306d38869ae6c7574a87449b2, reversing
    changes made to 79250046d9794f7bf244d213b8ea7056602fcfee.

--- CLE/CLE/router.py
+++ CLE/CLE/router.py
@@ -4,27 +4,25 @@ class TMRouter:
 
     # Attempts to read Module_TeamManagement models, got to CLE_Data
     def db_for_read(self, model, **hints):
-        if model._meta.app_label == 'Module_TeamManagement' or model._meta.app_label == 'Module_DeploymentMonitoring':
+        if model._meta.app_label == 'Module_TeamManagement':
             return 'CLE_Data'
         return None
 
     # Attempts to write Module_TeamManagement models, got to CLE_Data
     def db_for_write(self, model, **hints):
-        if model._meta.app_label == 'Module_TeamManagement' or model._meta.app_label == 'Module_DeploymentMonitoring':
+        if model._meta.app_label == 'Module_TeamManagement':
             return 'CLE_Data'
         return None
 
     # Allow relations if a model in the Module_TeamManagement app is involved
     def allow_relation(self, obj1, obj2, **hints):
         if obj1._meta.app_label == 'Module_TeamManagement' or \
-            obj2._meta.app_label == 'Module_TeamManagement' or \
-             obj1._meta.app_label == 'Module_DeploymentMonitoring' or \
-              obj2._meta.app_label == 'Module_DeploymentMonitoring':
+            obj2._meta.app_label == 'Module_TeamManagement':
             return True
         return None
 
     # Make sure the Module_TeamManagement app only appears in the 'CLE_Data' database
     def allow_migrate(self, db, app_label, model_name=None, **hints):
-        if app_label == 'Module_TeamManagement' or app_label == 'Module_DeploymentMonitoring':
+        if app_label == 'Module_TeamManagement':
             return db == 'CLE_Data'
         return None
