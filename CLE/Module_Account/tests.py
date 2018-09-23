19274016bda4fcd479b904c87c254e79036aae52
joel <joel.tay.2016@sis.smu.edu.sg>
2018-09-24 01:47:13 +0800

    Created test cases for automated test cases.

--- CLE/Module_Account/tests.py
+++ CLE/Module_Account/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
-
 # Create your tests here.
+from Module_Account.src import processLogin
+from django.contrib.auth.models import User
+
+class AccountModuleTest(TestCase):
+    def test_login(self):
+        a = processLogin.validate("admin","admin")
+        self.assertTrue(isinstance(a, User))
+        
+        b = processLogin.validate("admin","admi2n")
+        self.assertFalse(isinstance(b, User))
