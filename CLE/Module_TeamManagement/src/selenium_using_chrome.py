b205de1bfdb661fa3cad5ef5550f33067c387497
Martin Teo <martin.teo.2016@smu.edu.sg>
2018-09-24 17:41:15 +0800

    crontab selenium2

--- CLE/Module_TeamManagement/src/selenium_using_chrome.py
+++ CLE/Module_TeamManagement/src/selenium_using_chrome.py
@@ -17,13 +17,14 @@ class SeleniumTableau(unittest.TestCase):
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--window-size=1920x1080")
 
-        chromedriver = ""
+        chromedriver = "../../../setup/chromedriver"
         if platform == "linux" or platform == "linux2":
             chromedriver = "/home/ec2-user/Django_Application/MayhemFive/setup/chromedriver"
         elif platform == "darwin":
-            # OS X
+            chromedriver = "../../../setup/chromedriver"
         elif platform == "win32":
-        chromedriver = "../../../setup/chromedriver"
+            chromedriver = "../../../setup/chromedriver"
+
         self.driver = webdriver.Chrome(chromedriver,chrome_options=chrome_options)
 
         self.driver.implicitly_wait(30)
