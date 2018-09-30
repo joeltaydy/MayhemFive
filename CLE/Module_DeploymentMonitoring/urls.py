43be3b229cb8e9970c1f428bd97d40d4e343a1f7
Al <faried_94@hotmail.com>
2018-09-30 16:38:54 +0800

    Added neccessary URLs for frontend to call in their forms

--- CLE/Module_DeploymentMonitoring/urls.py
+++ CLE/Module_DeploymentMonitoring/urls.py
@@ -3,10 +3,12 @@ from Module_DeploymentMonitoring import views
 from django.urls import path
 
 urlpatterns = [
-    
-    path('instructor/ITOperationsLab/setup',views.faculty_Setup_Base,name='itopslab_setup'),
-    path('student/ITOperationsLab/deploy',views.student_Deploy_Base,name='itopslab_studeploy'),   
-    path('student/ITOperationsLab/monitor',views.ITOpsLabStudentMonitor,name='itopslab_stumonitor'),
-    path('instructor/ITOperationsLab/monitor',views.ITOpsLabMonitor,name='itopslab_monitor'),
-    path('instructor/ITOperationsLab/event',views.ITOpsLabEvent,name='itopslab_event'),
+    path('instructor/ITOperationsLab/setup/',views.faculty_Setup_Base,name='itopslab_setup'),
+    path('instructor/ITOperationsLab/setup/github/',views.faculty_Setup_GetGitHub,name='itopslab_setup_GitHub'),
+    path('instructor/ITOperationsLab/setup/awskeys/',views.faculty_Setup_GetAWSKeys,name='itopslab_setup_AWSKeys'),
+    path('instructor/ITOperationsLab/setup/ami/',views.faculty_Setup_ShareAMI,name='itopslab_setup_AMI'),
+    path('instructor/ITOperationsLab/monitor/',views.ITOpsLabMonitor,name='itopslab_monitor'),
+    path('instructor/ITOperationsLab/event/',views.ITOpsLabEvent,name='itopslab_event'),
+    path('student/ITOperationsLab/deploy/',views.student_Deploy_Base,name='itopslab_studeploy'),
+    path('student/ITOperationsLab/monitor/',views.ITOpsLabStudentMonitor,name='itopslab_stumonitor'),
 ]
