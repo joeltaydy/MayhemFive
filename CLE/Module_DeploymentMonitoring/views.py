8d6f9f8e7f2cac2471e5763d2b8719f3688e78c8
Al <faried_94@hotmail.com>
2018-10-25 00:53:10 +0800

    Added more debug lines

--- CLE/Module_DeploymentMonitoring/views.py
+++ CLE/Module_DeploymentMonitoring/views.py
@@ -367,7 +367,8 @@ def faculty_Setup_ShareAMI(requests):
         if choosen_account_list != None and len(choosen_account_list) > 0:
             imageObj = Image_Details.objects.get(imageId=image_id)
             current_account_list = [] if imageObj.sharedAccNum == None else imageObj.sharedAccNum
-
+            print(choosen_account_list)
+            print(current_account_list)
             # Step 1: ADD the account number to the image permission on AWS
             add_list = list(set(choosen_account_list)-set(current_account_list))
             print(add_list)
