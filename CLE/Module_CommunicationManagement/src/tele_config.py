3488d78156bed8466ea2a04fe4bbfba279012c6c
Al <faried_94@hotmail.com>
2018-11-09 18:31:45 +0800

    Update tele_config to check system

--- CLE/Module_CommunicationManagement/src/tele_config.py
+++ CLE/Module_CommunicationManagement/src/tele_config.py
@@ -3,8 +3,10 @@ import os
 # telethon configuration values
 API_ID = '367454'
 API_HASH = '1bf84fb9cec9b739bc9dc2a5fe97ee10'
-SESSION_FOLDER = os.path.join(os.getcwd(),'Module_CommunicationManagement/src/telegram_sessions')
-# SESSION_FOLDER = os.path.abspath('telegram_sessions')
+if 'posix' in os.name:
+    SESSION_FOLDER = os.path.join(os.getcwd(),'Module_CommunicationManagement/src/telegram_sessions')
+else:
+    SESSION_FOLDER = os.path.join(os.getcwd(),'Module_CommunicationManagement\\src\\telegram_sessions')
 ADMIN_SESSION_FILE = 'admin_login.session'
 ADMIN_USERNAME = 'admin_login'
 
