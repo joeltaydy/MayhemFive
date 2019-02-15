a8f587430ac4d1ac6577fc9bc2ae51eb05274284
joel <joel.tay.2016@sis.smu.edu.sg>
2019-02-15 12:50:05 +0800

    commit changes to enable database viewing for django admin

--- CLE/Module_DeploymentMonitoring/admin.py
+++ CLE/Module_DeploymentMonitoring/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from Module_DeploymentMonitoring.models import *
+
+class AWSCredentialsAdmin(admin.ModelAdmin):
+    # define which columns displayed in changelist
+    list_display = ('account_number', 'access_key', 'secret_access_key', 'image_details')
+    # add filtering by date
+    #list_filter = ('date',)
+    # add search field 
+    #search_fields = ['email', 'firstname']
+    def image_details(self,obj):
+        return "\n".join([image.imageId for image in  obj.imageDetails.all()])
+
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('imageName', 'teams')
+
+    def teams(self,obj):
+        return sharedAccNum
+
+class DeploymentPackageAdmin(admin.ModelAdmin):
+    list_display = ('deployment_name','deployment_link','shared_sections')
 
 # Register your models here.
+admin.site.register(AWS_Credentials,AWSCredentialsAdmin)
+admin.site.register(Image_Details,ImageAdmin)
+admin.site.register(Deployment_Package,DeploymentPackageAdmin)
--- CLE/Module_EventConfig/admin.py
+++ CLE/Module_EventConfig/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from Module_EventConfig.models import *
 # Register your models here.
+
+
+class EventAdmin(admin.ModelAdmin):
+    # define which columns displayed in changelist
+    list_display = ('id','event_type', 'event_startTime', 'event_endTime', 'event_recovery', 'server_ip')
+    # add filtering by date
+    #list_filter = ('date',)
+    # add search field 
+    def server_ip(self,obj):
+        return obj.server_details.IP_address
+
+
+admin.site.register(Event_Details, EventAdmin)
