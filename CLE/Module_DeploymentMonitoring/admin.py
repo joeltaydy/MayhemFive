ce7afa8f088256e5d82546df735eda2966a610e4
joel <joel.tay.2016@sis.smu.edu.sg>
2019-02-15 12:56:14 +0800

    edit image details to be displayed and remove of events conf models

--- CLE/Module_DeploymentMonitoring/admin.py
+++ CLE/Module_DeploymentMonitoring/admin.py
@@ -3,7 +3,8 @@ from Module_DeploymentMonitoring.models import *
 
 class AWSCredentialsAdmin(admin.ModelAdmin):
     # define which columns displayed in changelist
-    list_display = ('account_number', 'access_key', 'secret_access_key', 'image_details')
+    list_display = ('account_number', 'access_key', #'secret_access_key',
+     'image_details')
     # add filtering by date
     #list_filter = ('date',)
     # add search field 
@@ -12,10 +13,8 @@ class AWSCredentialsAdmin(admin.ModelAdmin):
         return "\n".join([image.imageId for image in  obj.imageDetails.all()])
 
 class ImageAdmin(admin.ModelAdmin):
-    list_display = ('imageName', 'teams')
+    list_display = ('imageName', 'imageId')
 
-    def teams(self,obj):
-        return obj.sharedAccNum
 
 class DeploymentPackageAdmin(admin.ModelAdmin):
     list_display = ('deployment_name','deployment_link','shared_sections')
